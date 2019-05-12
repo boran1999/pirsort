c57195be5dad9703084d70cd10b8e6c3d98b07f9
Anton Borisov <32795987+boran1999@users.noreply.github.com>
2019-05-12 07:41:47 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include "sort.h"
+#include <ctime>
+#include <stdlib.h>
+
+using namespace std;
+
+int main(void) {
+	/*
+	SortableVector<int> vec;
+	srand((unsigned)time(0));
+	for (int i = 0; i < 10; i++) {
+		vec.push_back(rand());
+		cout << vec[i]<<" ";
+	}
+	cout << endl;
+	vec.sort();
+	for (int i = 0; i < 10; i++) {	
+		cout << vec[i] << " ";
+	}
+	*/
+	char a[] = "AAAAA";
+	system("pause");
+	return 0;
+}
